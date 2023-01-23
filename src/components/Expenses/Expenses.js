a8ca2a48f184717e125767c9a7ff384c07fcf056
mauroef <cypress90@gmail.com>
2023-01-22 23:03:31 -0300

    67

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from './ExpenseItem';
-import ExpensesFilter from './ExpensesFilter';
-import Card from '../UI/Card';
-import './Expenses.css';
 import { useState } from 'react';
+import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
@@ -15,21 +15,13 @@ const Expenses = (props) => {
     (expenses) => expenses.date.getFullYear().toString() === filteredYear
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((i) => (
-      <ExpenseItem key={i.id} title={i.title} amount={i.amount} date={i.date} />
-    ));
-  }
-
   return (
     <Card className='expenses'>
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
