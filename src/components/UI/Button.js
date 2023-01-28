afa1ebbe9f6a73b7d3242f25379c37b606a3b41b
mauroef <cypress90@gmail.com>
2023-01-28 19:37:31 -0300

    97

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
     <button
       className={classes.button}
       type={props.type || 'button'}
-      onClick={props.onCLick}
+      onClick={props.onClick}
     >
       {props.children}
     </button>
