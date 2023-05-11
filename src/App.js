b1f782ef9eb0bdf9fbf188f72c4d6d5f20d2ca1c
mauroef <cypress90@gmail.com>
2023-05-10 23:23:56 -0300

    180 Using useEffect on requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     try {
       const response = await fetch('https://swapi.dev/api/films');
@@ -29,7 +29,35 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  // without useCallback
+  // const fetchMoviesHandler = async () => {
+  //   setIsLoading(true);
+  //   try {
+  //     const response = await fetch('https://swapi.dev/api/films');
+  //     if (!response.ok) {
+  //       throw new Error('Something went wrong');
+  //     }
+  //     const data = await response.json();
+  //     const transformedMovies = data.results.map((movieData) => {
+  //       return {
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date,
+  //       };
+  //     });
+  //     setMovies(transformedMovies);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>;
 
