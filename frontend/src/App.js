a2673b036b41347bcb9e2829dcdc7befeb916a0f
mauroef <cypress90@gmail.com>
2023-10-29 18:27:45 -0300

    Extracting error data & throwing responses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,17 +23,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
 import EventDetailPage from './pages/EventDetail';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
-import EventsRootLayout from './pages/EventsRoot';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,6 +4,11 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
   const events = data.events;
 
   return (
@@ -19,7 +24,10 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: 'Could not fecth events.' }; FIXME: there is a better way
+    throw new Response(JSON.stringify({ message: 'Could not fecth events.' }), {
+      status: 500,
+    });
   } else {
     return response;
   }
