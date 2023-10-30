9880da4bf40f85d0858863058de657bef8367d78
mauroef <cypress90@gmail.com>
2023-10-30 08:10:03 -0300

    Submitting data progammatically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from './pages/EventDetail';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
@@ -54,7 +55,11 @@ const router = createBrowserRouter([
             id: 'event-detail',
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailPage /> },
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
               { path: 'edit', element: <EditEventPage /> },
             ],
           },
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from 'react-router-dom';
+import { json, redirect, useRouteLoaderData } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
@@ -26,5 +26,18 @@ export async function loader({ request, params }) {
     );
   } else {
     return response;
+  }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.id;
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: 'Could not delete event.' }, { status: 500 });
+  } else {
+    return redirect('/events');
   }
 }
