9e04d2a1c360418e1571553c2b452b3eb239d726
mauroef <mauroef.dev@gmail.com>
2024-02-24 17:04:32 -0300

    Deferring data fetching with defer()

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -12,7 +12,10 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const events = await getAll();
-    res.json({ events: events });
+    // for defer testing
+    setTimeout(() => {
+      res.json({ events: events });
+    }, 2000);
   } catch (error) {
     next(error);
   }
