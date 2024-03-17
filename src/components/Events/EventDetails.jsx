6876cd408e271218710f106999d90e13b55576ce
mauroef <mauroef.dev@gmail.com>
2024-03-17 10:12:18 -0300

    Disabling automatic refetching after invalidations

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,103 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
+import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
 import Header from '../Header.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['events', id],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate } = useMutation({
+    queryKey: ['events'],
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none',
+      });
+      navigate('/events');
+    },
+  });
+
+  function handleDelete(id) {
+    mutate({ id });
+  }
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div id='event-datails-content' className='center'>
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id='event-datails-content' className='center'>
+        <ErrorBlock
+          title='An error occurred'
+          message={error.info?.message || 'Failed to fetch event.'}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button
+              onClick={() => {
+                handleDelete(data.id);
+              }}
+            >
+              Delete
+            </button>
+            <Link to='edit'>Edit</Link>
+          </nav>
+        </header>
+        <div id='event-details-content'>
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id='event-details-info'>
+            <div>
+              <p id='event-details-location'>{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id='event-details-description'>{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
       <Header>
-        <Link to="/events" className="nav-item">
+        <Link to='/events' className='nav-item'>
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      <article id='event-details'>{content}</article>
     </>
   );
 }
