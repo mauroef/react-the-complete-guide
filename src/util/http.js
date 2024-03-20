924c0fecab53643105bfe78b5ec4cfe48dad81f2
mauroef <mauroef.dev@gmail.com>
2024-03-20 18:59:30 -0300

    Using the query key as query function

--- src/util/http.js
+++ src/util/http.js
@@ -2,11 +2,15 @@ import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
 
-export async function fetchEvents({ searchTerm, signal }) {
+export async function fetchEvents({ searchTerm, signal, max }) {
   let url = 'http://localhost:3000/events';
 
-  if (searchTerm) {
+  if (searchTerm && max) {
+    url += '?search=' + searchTerm + '&max=' + max;
+  } else if (searchTerm) {
     url += `?search=${searchTerm}`;
+  } else if (max) {
+    url += '?max=' + max;
   }
 
   const response = await fetch(url, { signal: signal });
@@ -62,7 +66,9 @@ export async function fetchSelectableImages({ signal }) {
 }
 
 export async function fetchEvent({ id, signal }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the event');
