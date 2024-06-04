7aea49a65c5ae4a6c26c8def6b83727bc95ef1c2
mauroef <mauroef.dev@gmail.com>
2024-06-04 20:31:17 -0300

    Creating slug and sanitizing for xss protection

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import { shareMeal } from '@/lib/actions';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
@@ -11,37 +12,37 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <label htmlFor='name'>Your name</label>
+              <input type='text' id='name' name='name' required />
             </p>
             <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <label htmlFor='email'>Your email</label>
+              <input type='email' id='email' name='email' required />
             </p>
           </div>
           <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' name='title' required />
           </p>
           <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <label htmlFor='summary'>Short Summary</label>
+            <input type='text' id='summary' name='summary' required />
           </p>
           <p>
-            <label htmlFor="instructions">Instructions</label>
+            <label htmlFor='instructions'>Instructions</label>
             <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
+              id='instructions'
+              name='instructions'
+              rows='10'
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label={'Your image'} name={'image'}/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <button type='submit'>Share Meal</button>
           </p>
         </form>
       </main>
