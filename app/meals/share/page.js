07333db166141ebe036b719802f20745692bf1d2
mauroef <mauroef.dev@gmail.com>
2024-06-05 19:44:09 -0300

    Triggering cache revalidations

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor='name'>Your name</label>
@@ -41,7 +46,8 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label={'Your image'} name={'image'}/>
+          <ImagePicker label={'Your image'} name={'image'} />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
