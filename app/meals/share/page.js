de4e134b09cd79d4ca729b35f4d2f27f3ff39d64
mauroef <mauroef.dev@gmail.com>
2024-06-05 19:11:01 -0300

    Adding server-side inpput validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,6 @@
-import { shareMeal } from '@/lib/actions';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label={'Your image'} name={'image'}/>
           <p className={classes.actions}>
-            <button type='submit'>Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
