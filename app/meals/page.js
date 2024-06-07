b9cb9340dfa4e7cea9b66ecbf4168e0befb2830e
mauroef <mauroef.dev@gmail.com>
2024-06-07 19:11:00 -0300

    Adding static metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious recipes on NextLevel Food.',
+};
+
 async function Meals() {
   const meals = await getMeals();
 
